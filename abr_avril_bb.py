2f5b7bdf206b20645b09549c09224bfe2e588495
NathanGrgr <126341708+NathanGrgr@users.noreply.github.com>
2024-04-17 09:56:36 +0200

    Add files via upload

---
+++ abr_avril_bb.py
@@ -0,0 +1,37 @@
+﻿
+class ABR:
+      def __init__(self,value=0,gauche=None,droite=None):
+          self.value=value
+          self.gauche=gauche
+          self.droite=droite
+
+
+      def insertion(self,valeur):
+          if valeur>self.value:
+
+
+
+
+             courant=self.droite
+             while courant.droite!=None:
+                   if valeur>self.value:
+                      courant=self.droite.droite
+
+             if courant.droite==None:
+                self.droite=ABR(valeur)
+          """
+          elif valeur<self.value:
+
+
+
+
+          elif valeur<self.value and self.droite==None:
+               arbre.gauche=ABR(valeur)
+          """
+
+
+if __name__=="__main__":
+   ag=ABR(3)
+   ad=ABR(6)
+   arbre=ABR(5,ag,ad)
+   arbre.insertion(10)
