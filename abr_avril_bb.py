1c7e0007f48d24a57c73a0d14de0c266b68377f7
NathanGrgr <126341708+NathanGrgr@users.noreply.github.com>
2024-04-23 21:34:35 +0200

    Update abr_avril_bb.py

--- abr_avril_bb.py
+++ abr_avril_bb.py
@@ -1,10 +1,39 @@
 compt=0
+l_total=[]
 
 class ABR:
       def __init__(self,value=0,gauche=None,droite=None):
           self.value=value
           self.gauche=gauche
           self.droite=droite
+      
+
+      """
+      def __repr__(self) -> str:
+        global l_total
+        l_total.append(self.value)
+        self.droite.__repr__()
+        self.gauche.__repr__()
+        print(l_total)
+        init=l_total[0]
+        l_constru=[]
+        a=0
+        for i in range(len(l_total)):
+            if l_total[i]==init:
+                l_constru.append(l_total[i])
+                a+=1
+            elif l_total[i]>init:
+                l_constru.append(l_total[i])
+                a+=1
+                while l_total[a]>init:
+                    pass
+      """
+
+      def __repr__(self) -> str:
+        global l_total
+        l_total.append(self.value)
+        self.droite.__repr__()
+        self.gauche.__repr__()
 
 
       def insertion(self,valeur):
@@ -40,10 +69,43 @@ class ABR:
       def affichage_console(self,arbre):
           pass
 
+def identif_enfants():
+    init=l_total[0]
+    parent_droite=None
+    parent_gauche=None
+    block_gauche=False
+    block_droite=False
+    for i in range(len(l_total)):
+        #print(l_total[i])
+
+        if l_total[i]>init and block_droite==False:
+            parent_droite=(l_total[i],i)
+            block_droite=True
+        if l_total[i]<init and block_gauche==False:
+            parent_gauche=(l_total[i],i)
+            block_gauche=True
+
+    return(parent_droite,parent_gauche)
+
+
+def suite():
+    parent_droite,parent_gauche=identif_enfants()
+    print(l_total)
+    print(parent_droite,parent_gauche)
+    liste_1=l_total[parent_droite[1]:parent_gauche[1]]
+    print(liste_1)
+
+
+
 if __name__=="__main__":
    ag=ABR(3)
-   ad=ABR(6)
+   ad=ABR(7)
    arbre=ABR(5,ag,ad)
    arbre.insertion(10)
+   arbre.insertion(2)
    arbre.insertion(4)
-   print(arbre.recherche(10))
+   arbre.insertion(6)
+   arbre.insertion(1)
+   #print(arbre.recherche(10))
+   print(arbre.__repr__())
+   print(suite())
